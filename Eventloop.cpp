93654e4fe11006d26b799d2c3b236f857a8ffb3e
SHELLEY11 <Shelley_2020@163.com>
2022-08-01 20:40:51 +0800

    try

--- Eventloop.cpp
+++ Eventloop.cpp
@@ -1,175 +1,58 @@
-// #include <sys/eventfd.h>
-// #include <unistd.h>
-// #include <iostream>
-
-// int main() {
-//     int efd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
-//     eventfd_write(efd, 2);
-//     eventfd_t count;
-//     eventfd_read(efd, &count);
-//     std::cout << count << std::endl;
-//     close(efd);
-// }
-
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <stdint.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
 #include <sys/eventfd.h>
+#include <signal.h>
 #include <sys/epoll.h>
-
-int efd = -1;
-
-void *read_thread(void *dummy)
-{
-    int ret = 0;
-    uint64_t count = 0;
-    int ep_fd = -1;
-    struct epoll_event events[10];
-
-    if (efd < 0)
-    {
-        printf("efd not inited.\n");
-        goto fail;
+ 
+static const int EPOLL_MAX_EVENTS = 16;
+ 
+void* worker(void* p_data) {
+    int* event_fd = (int *)p_data;
+    printf("worker thread start..., event_fd = %d\n", *event_fd);
+    int number = 1;
+    for (int i = 1; i < 10; i++) {
+        int size = eventfd_write(*event_fd, (eventfd_t)number);
+        printf("i = %d, size = %d, errno = %d\n", i, size, errno);
     }
-
-    ep_fd = epoll_create(1024);
-    if (ep_fd < 0)
-    {
-        perror("epoll_create fail: ");
-        goto fail;
-    }
-
-    {
-        struct epoll_event read_event;
-
-        read_event.events = EPOLLHUP | EPOLLERR | EPOLLIN;
-        read_event.data.fd = efd;
-
-        ret = epoll_ctl(ep_fd, EPOLL_CTL_ADD, efd, &read_event);
-        if (ret < 0)
-        {
-            perror("epoll ctl failed:");
-            goto fail;
-        }
-    }
-
-    while (1)
-    {
-        ret = epoll_wait(ep_fd, &events[0], 10, 5000);
-        if (ret > 0)
-        {
-            int i = 0;
-            for (; i < ret; i++)
-            {
-                if (events[i].events & EPOLLHUP)
-                {
-                    printf("epoll eventfd has epoll hup.\n");
-                    goto fail;
-                }
-                else if (events[i].events & EPOLLERR)
-                {
-                    printf("epoll eventfd has epoll error.\n");
-                    goto fail;
-                }
-                else if (events[i].events & EPOLLIN)
-                {
-                    int event_fd = events[i].data.fd;
-                    ret = read(event_fd, &count, sizeof(count));
-                    if (ret < 0)
-                    {
-                        perror("read fail:");
-                        goto fail;
-                    }
-                    else
-                    {
-                        struct timeval tv;
-
-                        gettimeofday(&tv, NULL);
-                        printf("success read from efd, read %d bytes(%llu) at %lds %ldus\n",
-                               ret, count, tv.tv_sec, tv.tv_usec);
-                    }
-                }
-            }
-        }
-        else if (ret == 0)
-        {
-            /* time out */
-            printf("epoll wait timed out.\n");
-            break;
-        }
-        else
-        {
-            perror("epoll wait error:");
-            goto fail;
-        }
-    }
-
-fail:
-    if (ep_fd >= 0)
-    {
-        close(ep_fd);
-        ep_fd = -1;
-    }
-
+    printf("worker thread stop...\n");
+    // sleep(1);
     return NULL;
 }
 
+// struct dog{
+//     char *name;
+//     int  age;
+// }dog1 = {"Tom", 2};
+ 
+int main() {
+    eventfd_t count;
+    struct epoll_event event, events[EPOLL_MAX_EVENTS];
+    pthread_t p_main = pthread_self();
+    int epoll_fd = epoll_create1(EPOLL_CLOEXEC);
 
-int main(int argc, char *argv[])
-{
-    pthread_t pid = 0;
-    uint64_t count = 0;
-    int ret = 0;
-    int i = 0;
-
-    efd = eventfd(0, 0);
-    if (efd < 0)
-    {
-        perror("eventfd failed.");
-        goto fail;
-    }
-
-    ret = pthread_create(&pid, NULL, read_thread, NULL);
-    if (ret < 0)
-    {
-        perror("pthread create:");
-        goto fail;
-    }
-
-    for (i = 0; i < 5; i++)
-    {
-        count = 4;
-        ret = write(efd, &count, sizeof(count));
-        if (ret < 0)
-        {
-            perror("write event fd fail:");
-            goto fail;
+    int event_fd = eventfd(0, EFD_NONBLOCK | EFD_SEMAPHORE);
+    printf("event_fd = %d\n", event_fd);
+    event.data.fd = event_fd;
+    event.events = EPOLLIN;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, event_fd, &event);
+ 
+    pthread_t p_worker;
+    pthread_create(&p_worker, NULL, worker, &event_fd);
+ 
+    for (;;) {
+        // printf("main thread wait events...\n");
+        int len = epoll_wait(epoll_fd, events, EPOLL_MAX_EVENTS, -1);
+        // printf("len of epoll_wait = %d\n", len);
+        for (int i = 0; i < len; i++) {
+            if (events[i].events == EPOLLIN) {
+                eventfd_read(event_fd, &count);
+                printf("count = %d\n", count);
+            }
         }
-        else
-        {
-            struct timeval tv;
-
-            gettimeofday(&tv, NULL);
-            printf("success write to efd, write %d bytes(%llu) at %lds %ldus\n",
-                   ret, count, tv.tv_sec, tv.tv_usec);
-        }
-
-        sleep(1);
     }
-
-fail:
-    if (0 != pid)
-    {
-        pthread_join(pid, NULL);
-        pid = 0;
-    }
-
-    if (efd >= 0)
-    {
-        close(efd);
-        efd = -1;
-    }
-    return ret;
+ 
+   return 0;
 }
