8fc5d1d8acdcfa144732bef024f6fa05b8fe8014
SHELLEY11 <Shelley_2020@163.com>
2022-08-16 17:17:52 +0800

    pipeshell

---
+++ Pipe_Shell/Pipe_shell.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<signal.h>
+#include<dirent.h>
+#include<fcntl.h>
+#include<ctype.h>
+
+#ifndef __ PipeShell_H__
+#define __PipeShell_H__
+
+class PipeShell{
+
+};
+#endif 
